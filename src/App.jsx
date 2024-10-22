bc653118391f6c51edb01cdb4147a41f37f44c24
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-10-22 17:20:51 +0200

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/user/*" element={<Contact />} />
       </Routes>
     </Router>
   );
