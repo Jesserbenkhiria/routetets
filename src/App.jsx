684a0957ffe04ef8adbfa57158a197fcce56b21d
Jesserbenkhiria <jesserbenkhiria911@gmail.com>
2024-10-22 17:53:00 +0200

    sss

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,23 @@ function Contact() {
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/user">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <Routes>
-        <Route path="/" element={<Contact />} />
+        <Route path="/" element={<Home />} />
         <Route path="/user/*" element={<User />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
